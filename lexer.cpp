4f4292ff08faa000cc63e3caa6c3baa02e6e5b90
Bhavyesh Desai <b.desai7@outlook.com>
2021-02-19 19:10:10 +0530

    update lexer

--- lexer.cpp
+++ lexer.cpp
@@ -1,121 +1,172 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<pair<string, int>> v{
-    {"int", 100},
-    {"if", 101},
-    {"else", 102},
-    {"while", 103},
-    {"return", 104},
-    {"main", 105},
-    {"(", 110},
-    {")", 111},
-    {"{", 112},
-    {"}", 113},
-    {"[", 114},
-    {"]", 115},
-    {";", 116},
-    {",", 117},
-    {"+", 120},
-    {"*", 121},
-    {"<", 122},
-    {"!", 123},
-    {"=", 124},
+#define LPAREN 110
+#define RPAREN 111
+#define LBRACE 112
+#define RBRACE 113
+#define SEMICOL 114
+#define COMMA 115
+#define OP_ADD 120
+#define OP_MUL 121
+#define LESS 122
+#define NOT 123
+#define ASSIGN 124
+#define IDENTIFIER 130
+#define INTEGER_LITERAL 131
+#define END 132
+
+class Scanner {
+    int line;
+    bool bufferCharLoaded;
+    char bufferChar;
+    ifstream program;
+    vector<pair<string, int>> v;
+    char peekChar()
+    {
+        if(!bufferCharLoaded)
+        {
+            bufferCharLoaded = true;
+            program >> noskipws >> bufferChar;
+        }
+        return bufferChar;
+    }
+    char getChar()
+    {
+        if(!bufferCharLoaded)
+        {
+            bufferCharLoaded = true;
+            program >> noskipws >> bufferChar;
+        }
+        bufferCharLoaded = false;
+        if(bufferChar == '\n')
+            line++;
+        return bufferChar;
+    }
+    void skipLine()
+    {
+        while(!program.eof() && getChar() != '\n')
+            ;
+    }
+    int DFA(int state, string &token)
+    {
+        if(state == 0)
+        {
+            char c = getChar();
+            if(program.eof())
+                return END;
+            if(c == '#')
+            {
+                skipLine();
+                return DFA(state, token);
+            }
+            if(c == ' ' || c == '\n')
+                return DFA(state, token);
+            token += c;
+            if(c == '(')
+                return LPAREN;
+            if(c == ')')
+                return RPAREN;
+            if(c == '{')
+                return LBRACE;
+            if(c == '}')
+                return RBRACE;
+            if(c == ';')
+                return SEMICOL;
+            if(c == ',')
+                return COMMA;
+            if(c == '+')
+                return OP_ADD;
+            if(c == '*')
+                return OP_MUL;
+            if(c == '<')
+                return LESS;
+            if(c == '!')
+                return NOT;
+            if(c == '=')
+                return ASSIGN;
+            if(c >= '0' && c <= '9')
+                return DFA(1, token);
+            if((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+                return DFA(2, token);
+            throw "Error, cannot process " + string(1, c) + " as beggining of a token!!!";
+        }
+        else if(state == 1)
+        {
+            char c = peekChar();
+            if(program.eof())
+                return INTEGER_LITERAL;
+            if(c >= '0' && c <= '9')
+            {
+                c = getChar();
+                token += c;
+                return DFA(1, token);
+            }
+            return INTEGER_LITERAL;
+        }
+        else
+        {
+            char c = peekChar();
+            if(program.eof())
+            {
+                for(auto &k: v)
+                    if(k.first == token)
+                        return k.second;
+                return IDENTIFIER;
+            }
+            if((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9'))
+            {
+                c = getChar();
+                token += c;
+                return DFA(2, token);
+            }
+            for(auto &k: v)
+                if(k.first == token)
+                    return k.second;
+            return IDENTIFIER;
+        }
+    }
+    public:
+    Scanner(string programFile)
+    : line(1)
+    , bufferCharLoaded(false)
+    , program(programFile)
+    , v
+        {
+            {"int", 100},
+            {"if", 101},
+            {"else", 102},
+            {"while", 103}
+        }
+    {}
+    tuple<int, string, int> getToken()
+    {
+        string token;
+        int id = DFA(0, token);
+        if(id == END)
+            token = "FILE_END_REACHED";
+        return {id, token, line};
+    }
 };
-//130 -> identifier
-//131 -> integer literal
-
-int DFA(string &s, int index, int state)
-{
-    if(state == 0)
-    {
-        if((s[0] >= 'a' && s[0] <= 'z') || (s[0] >= 'A' && s[0] <= 'Z'))
-            return DFA(s, index+1, 1);
-        else if((s[0] >= '0' && s[0] <= '9') || s[0] == '+' || s[0] == '-')
-            return DFA(s, index+1, 2);
-        else
-            throw "Invalid Token: " + s;
-    }
-    else if(state == 1)
-    {
-        if(index == (int)s.size())
-            return 130;
-        else if((s[index] >= 'a' && s[index] <= 'z') || (s[index] >= 'A' && s[index] <= 'Z') || (s[index] >= '0' && s[index] <= '9') || s[index] == '_')
-            return DFA(s, index+1, 1);
-        else
-            throw "Invalid Token: " + s;
-    }
-    else if(state == 2)
-    {
-        if(index == (int)s.size())
-            return 131;
-        else if(s[index] >= '0' && s[index] <= '9')
-            return DFA(s, index+1, 2);
-        else
-            throw "Invalid Token: " + s;
-    }
-    throw "Invalid Token: " + s;
-}
-
-void tokenize(string s, int line)
-{
-    string t = "";
-    for(auto &k: v)
-        if(k.first == s)
-            t = to_string(k.second);
-    if(t == "")
-        t = to_string(DFA(s, 0, 0));
-    cout << "Token " << t << ", string " << s << ", line number " << line << "\n";
-}
-
-void process(string &s, int line)
-{
-    if(s != "")
-        tokenize(s, line);
-    s = "";
-}
 
 int32_t main()
 {
     ios::sync_with_stdio(false);
     cin.tie(0);
-    ifstream program("program.xyz");
-    int line = 1;
-    char c;
-    string s = "";
-    try {
-        while(program >> noskipws >> c)
-        {
-            //handle + seperately
-            if(c == '#')
-            {
-                while((program >> noskipws >> c))
-                    if(c == '\n')
-                        break;
-            }
-            else if(c == '(' || c == ')' || c == '{' || c == '}' || c == '[' || c == ']' || c == ';' || c == ',')
-            {
-                process(s, line);
-                s += c;
-                process(s, line);
-            }
-            else if(c == '+' || c == '*' || c == '<' || c == '!' || c == '=')
-            {
-                process(s, line);
-                s += c;
-                process(s, line);
-            }
-            else if(c == ' ' || c == '\n')
-                process(s, line);
-            else
-                s += c;
-            if(c == '\n')
-                line++;
-        }
-        process(s, line);
-    }
-    catch(string err)
+    Scanner scanner("program.c++++");
+    while(true)
     {
-        cout << err << "\n";
+        try {
+            tuple<int, string, int> token = scanner.getToken();
+            int id, line;
+            string s;
+            tie(id, s, line) = token;
+            cout << id << " " << s << " " << line << "\n";
+            if(id == END)
+                break;
+        }
+        catch(string error) {
+            cout << error << "\n";
+        }
     }
 }
