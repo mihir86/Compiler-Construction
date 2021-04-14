19d262e884ce72e919bf64928bdc73e2dbeb7844
Bhavyesh Desai <b.desai7@outlook.com>
2021-04-14 21:33:42 +0530

    add docs

--- include/parseTreeNode.hpp
+++ include/parseTreeNode.hpp
@@ -2,12 +2,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+//parseTreeNode class used to represent a parse tree node
 class parseTreeNode {
+    //the token stored in the node
     string token;
+    //list of children of the node in the parse tree
     vector<parseTreeNode*> children;
     public:
+    //initialise the node with a string
     parseTreeNode(string _token);
+    //add a node to the list of children
     void addNode(parseTreeNode *node);
+    //perform corrective measures to ensure the order of children in the tree is correct
     void correctOrder();
+    //print the parse tree in a bracketed format
     void printTree();
 };
--- include/parser.hpp
+++ include/parser.hpp
@@ -4,16 +4,34 @@
 #include <parseTreeNode.hpp>
 using namespace std;
 
+//Parser class that can be initialised with a file
 class Parser {
+    //scanner object to get stream of tokens
     Scanner scanner;
+    //parse table stored in the memory for quick access
     map<pair<int, int>, string> parseTable;
+    //information about the BNF moves
+    //is stored in the reduceTable
+    //gives information about the non terminal on the LHS
+    //along with the number of items on the RHS
     map<int, pair<int, int>> reduceTable;
+    //stack used in the parsing process
+    //the first argument is the token number of the token
+    //second argument is the state number
+    //third argument is the parse tree node associated with the token
     stack<tuple<int, int, parseTreeNode*>> s;
+    //errorStack is used in the error handling part of the parser
+    //it is used to push input tokens if they're temporarily not needed
     stack<tuple<int, string, int>> errorStack;
+    //get token works in sync with scanner and errorStack to return the next token
     tuple<int, string, int> getToken(int &flag);
+    //getString is used to get the relevant string to be stored in the parse tree nodes
     map<int, string> getString;
     public:
+    //constructor to initialise the file
     Parser(char* fileName);
+    //print all tokens, used to debug the class
     void printTokens();
+    //used to perform parsing on the given input .c++++ file
     void LALR1();
 };
